57c74c41a4924d9edc06d9776fa80e0fe79ad5d4
mg <mg@mg.com>
2023-03-12 20:15:34 -0400

    init

---
+++ src/complete_coverage.cpp
@@ -0,0 +1,308 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Robert Bosch LLC.
+ *  Copyright (c) 2015-2016, Jiri Horner.
+ *  Copyright (c) 2021, Carlos Alvarez, Juan Galvis.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Jiri Horner nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************/
+
+#include "complete_coverage_planner/complete_coverage.hpp"
+
+#include <thread>
+
+inline static bool same_point(const geometry_msgs::msg::Point& one,
+                              const geometry_msgs::msg::Point& two)
+{
+  double dx = one.x - two.x;
+  double dy = one.y - two.y;
+  double dist = sqrt(dx * dx + dy * dy);
+  return dist < 0.01;
+}
+
+namespace complete_coverage
+{
+CompleteCoverage::CompleteCoverage()
+  : Node("complete_coverage_node")
+  , tf_buffer_(this->get_clock())
+  , tf_listener_(tf_buffer_)
+  , costmap_client_(*this, &tf_buffer_)
+  , prev_distance_(0)
+  , last_markers_count_(0)
+{
+  double timeout;
+  double min_frontier_size;
+  this->declare_parameter<float>("planner_frequency", 1.0);
+  this->declare_parameter<float>("progress_timeout", 30.0);
+  this->declare_parameter<bool>("visualize", false);
+  this->declare_parameter<float>("potential_scale", 1e-3);
+  this->declare_parameter<float>("orientation_scale", 0.0);
+  this->declare_parameter<float>("gain_scale", 1.0);
+  this->declare_parameter<float>("min_frontier_size", 0.5);
+  this->declare_parameter<bool>("return_to_init", false);
+
+  this->get_parameter("planner_frequency", planner_frequency_);
+  this->get_parameter("progress_timeout", timeout);
+  this->get_parameter("visualize", visualize_);
+  this->get_parameter("potential_scale", potential_scale_);
+  this->get_parameter("orientation_scale", orientation_scale_);
+  this->get_parameter("gain_scale", gain_scale_);
+  this->get_parameter("min_frontier_size", min_frontier_size);
+  this->get_parameter("return_to_init", return_to_init_);
+  this->get_parameter("robot_base_frame", robot_base_frame_);
+
+  progress_timeout_ = timeout;
+  move_base_client_ =
+      rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(
+          this, ACTION_NAME);
+
+  search_ = frontier_exploration::FrontierSearch(costmap_client_.getCostmap(),
+                                                 potential_scale_, gain_scale_,
+                                                 min_frontier_size);
+
+  if (visualize_) {
+    marker_array_publisher_ =
+        this->create_publisher<visualization_msgs::msg::MarkerArray>("complete_coverage/"
+                                                                     "frontier"
+                                                                     "s",
+                                                                     10);
+  }
+
+  // Subscription to resume or stop exploration
+  resume_subscription_ = this->create_subscription<std_msgs::msg::Bool>(
+      "complete_coverage/resume", 10,
+      std::bind(&CompleteCoverage::resumeCallback, this, std::placeholders::_1));
+
+  RCLCPP_INFO(logger_, "Waiting to connect to move_base nav2 server");
+  move_base_client_->wait_for_action_server();
+  RCLCPP_INFO(logger_, "Connected to move_base nav2 server");
+
+  if (return_to_init_) {
+    RCLCPP_INFO(logger_, "Getting initial pose of the robot");
+    geometry_msgs::msg::TransformStamped transformStamped;
+    std::string map_frame = costmap_client_.getGlobalFrameID();
+    try {
+      transformStamped = tf_buffer_.lookupTransform(
+          map_frame, robot_base_frame_, tf2::TimePointZero);
+      initial_pose_.position.x = transformStamped.transform.translation.x;
+      initial_pose_.position.y = transformStamped.transform.translation.y;
+      initial_pose_.orientation = transformStamped.transform.rotation;
+    } catch (tf2::TransformException& ex) {
+      RCLCPP_ERROR(logger_, "Couldn't find transform from %s to %s: %s",
+                   map_frame.c_str(), robot_base_frame_.c_str(), ex.what());
+      return_to_init_ = false;
+    }
+  }
+
+}
+
+CompleteCoverage::~CompleteCoverage()
+{
+  stop();
+}
+
+void CompleteCoverage::resumeCallback(const std_msgs::msg::Bool::SharedPtr msg)
+{
+  if (msg->data) {
+    resume();
+  } else {
+    stop();
+  }
+}
+
+void CompleteCoverage::visualizeFrontiers(
+    const std::vector<frontier_exploration::Frontier>& frontiers)
+{
+
+}
+
+void CompleteCoverage::makePlan()
+{
+  // find frontiers
+  auto pose = costmap_client_.getRobotPose();
+  // get frontiers sorted according to cost
+  auto frontiers = search_.searchFrom(pose.position);
+  // find non blacklisted frontier
+  auto frontier =
+      std::find_if_not(frontiers.begin(), frontiers.end(),
+                       [this](const frontier_exploration::Frontier& f) {
+                         return goalOnBlacklist(f.centroid);
+                       });
+  if (frontier == frontiers.end()) {
+    RCLCPP_WARN(logger_, "All frontiers traversed/tried out, stopping.");
+    stop(true);
+    return;
+  }
+  geometry_msgs::msg::Point target_position = frontier->centroid;
+
+  // time out if we are not making any progress
+  bool same_goal = same_point(prev_goal_, target_position);
+
+  prev_goal_ = target_position;
+  if (!same_goal || prev_distance_ > frontier->min_distance) {
+    // we have different goal or we made some progress
+    last_progress_ = this->now();
+    prev_distance_ = frontier->min_distance;
+  }
+  // black list if we've made no progress for a long time
+  if ((this->now() - last_progress_ >
+      tf2::durationFromSec(progress_timeout_)) && !resuming_) {
+    frontier_blacklist_.push_back(target_position);
+    RCLCPP_DEBUG(logger_, "Adding current goal to black list");
+    makePlan();
+    return;
+  }
+
+  // ensure only first call of makePlan was set resuming to true
+  if (resuming_) {
+    resuming_ = false;
+  }
+
+  // we don't need to do anything if we still pursuing the same goal
+  if (same_goal) {
+    return;
+  }
+
+  RCLCPP_DEBUG(logger_, "Sending goal to move base nav2");
+
+  // send goal to move_base if we have something new to pursue
+  auto goal = nav2_msgs::action::NavigateToPose::Goal();
+  goal.pose.pose.position = target_position;
+  goal.pose.pose.orientation.w = 1.;
+  goal.pose.header.frame_id = costmap_client_.getGlobalFrameID();
+  goal.pose.header.stamp = this->now();
+
+  auto send_goal_options =
+      rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions();
+  // send_goal_options.goal_response_callback =
+  // std::bind(&CompleteCoverage::goal_response_callback, this, _1);
+  // send_goal_options.feedback_callback =
+  //   std::bind(&CompleteCoverage::feedback_callback, this, _1, _2);
+  send_goal_options.result_callback =
+      [this,
+       target_position](const NavigationGoalHandle::WrappedResult& result) {
+        reachedGoal(result, target_position);
+      };
+  move_base_client_->async_send_goal(goal, send_goal_options);
+}
+
+void CompleteCoverage::returnToInitialPose()
+{
+  RCLCPP_INFO(logger_, "Returning to initial pose.");
+  auto goal = nav2_msgs::action::NavigateToPose::Goal();
+  goal.pose.pose.position = initial_pose_.position;
+  goal.pose.pose.orientation = initial_pose_.orientation;
+  goal.pose.header.frame_id = costmap_client_.getGlobalFrameID();
+  goal.pose.header.stamp = this->now();
+
+  auto send_goal_options =
+      rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions();
+  move_base_client_->async_send_goal(goal, send_goal_options);
+}
+
+void CompleteCoverage::reachedGoal(const NavigationGoalHandle::WrappedResult& result,
+                          const geometry_msgs::msg::Point& frontier_goal)
+{
+  switch (result.code) {
+    case rclcpp_action::ResultCode::SUCCEEDED:
+      RCLCPP_DEBUG(logger_, "Goal was successful");
+      break;
+    case rclcpp_action::ResultCode::ABORTED:
+      RCLCPP_DEBUG(logger_, "Goal was aborted");
+      frontier_blacklist_.push_back(frontier_goal);
+      RCLCPP_DEBUG(logger_, "Adding current goal to black list");
+      // If it was aborted probably because we've found another frontier goal,
+      // so just return and don't make plan again
+      return;
+    case rclcpp_action::ResultCode::CANCELED:
+      RCLCPP_DEBUG(logger_, "Goal was canceled");
+      // If goal canceled might be because exploration stopped from topic. Don't make new plan.
+      return;
+    default:
+      RCLCPP_WARN(logger_, "Unknown result code from move base nav2");
+      break;
+  }
+  // find new goal immediately regardless of planning frequency.
+  // execute via timer to prevent dead lock in move_base_client (this is
+  // callback for sendGoal, which is called in makePlan). the timer must live
+  // until callback is executed.
+  // oneshot_ = relative_nh_.createTimer(
+  //     ros::Duration(0, 0), [this](const ros::TimerEvent&) { makePlan(); },
+  //     true);
+
+  // Because of the 1-thread-executor nature of ros2 I think timer is not
+  // needed.
+  makePlan();
+}
+
+void CompleteCoverage::start()
+{
+  RCLCPP_INFO(logger_, "Exploration started.");
+}
+
+void CompleteCoverage::stop(bool finished_exploring)
+{
+  RCLCPP_INFO(logger_, "Exploration stopped.");
+  move_base_client_->async_cancel_all_goals();
+  exploring_timer_->cancel();
+
+  if (return_to_init_ && finished_exploring) {
+    returnToInitialPose();
+  }
+}
+
+void CompleteCoverage::resume()
+{
+  resuming_ = true;
+  RCLCPP_INFO(logger_, "Exploration resuming.");
+  // Reactivate the timer
+  exploring_timer_->reset();
+  // Resume immediately
+  exploring_timer_->execute_callback();
+}
+
+}  // namespace complete_coverage
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  // ROS1 code
+  /*
+  if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,
+                                     ros::console::levels::Debug)) {
+    ros::console::notifyLoggerLevelsChanged();
+  } */
+  rclcpp::spin(
+      std::make_shared<complete_coverage::CompleteCoverage>());  // std::move(std::make_unique)?
+  rclcpp::shutdown();
+  return 0;
+}
